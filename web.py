9e0f5fae966ef35b1af520f5b366b20844d2769f
Swan <contact@swan.moe>
2017-11-17 20:46:02 -0500

    Initial Commit

---
+++ web.py
@@ -0,0 +1,21 @@
+from flask import Flask, make_response, redirect, request, render_template, url_for, flash
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+app = Flask(__name__)
+app.secret_key = ''
+
+@app.route('/')
+def index():
+
+    return ''
+
+@app.errorhandler(404)
+def not_found(error):
+    return '404'
+
+
+if __name__ == "__main__":
+    app.run(**config)
