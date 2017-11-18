9d72a8503da2d829570d38bc8f79b34dc0a5b9e7
Daniel Dimitrov <bgdppeu@gmail.com>
2017-11-19 00:24:46 +0200

    Not close to done...

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, redirect, request, render_template, url_for, flash
 import requests
-import datetime
+from datetime import datetime
 import json
 from helpers import mysql, Privileges
 
@@ -8,7 +8,7 @@ with open("config.json", "r") as f:
     config = json.load(f)
 
 app = Flask(__name__)
-app.secret_key = ''
+app.secret_key = 'betterrapisawesome'
 
 
 @app.route('/oauth/ripple/')
@@ -55,15 +55,6 @@ def ripple_aouth():
     return red
 
 
-def is_login():
-    ACCESS_TOKEN = request.cookies.get('ACCESS_TOKEN')
-
-    if ACCESS_TOKEN:
-        return True
-
-    return False
-
-
 def get_user():
     connection, cursor = mysql.connect()
 
@@ -78,11 +69,18 @@ def get_user():
     else:
         return 'ERROR'
 
-def api_user_privileges(user_id):
 
+def api_user_username(user_id):
+    user = requests.get('https://ripple.moe/api/v1/users', params={'id': user_id}).json()
+
+    return user['username']
+
+
+def api_user_privileges(user_id):
     user = requests.get('https://ripple.moe/api/v1/users', params={'id': user_id}).json()
 
     return user['privileges']
+
 
 def get_privileges(p):
     text = ''
@@ -110,6 +108,20 @@ def get_privileges(p):
 
     return text, perm
 
+def is_login(check_for_admin = False):
+    ACCESS_TOKEN = request.cookies.get('ACCESS_TOKEN')
+
+    if check_for_admin:
+        p, perm = get_privileges(api_user_privileges())
+
+
+
+        return ''
+
+    if ACCESS_TOKEN:
+        return True
+
+    return False
 
 @app.route('/oauth/ripple/logout/')
 def ripple_logout():
@@ -154,10 +166,11 @@ def home():
     u = get_user()
     p, perm = get_privileges(api_user_privileges(u['user_id']))
 
-    return render_template('home.html', user=get_user(), p=p, perm=perm)
+    return render_template('home.html', user=api_user_username(u['user_id']), p=p,
+                           perm=perm)
 
 
-@app.route('/banappeal/')
+@app.route('/banappeal/', methods=['GET', 'POST'])
 def request_banappeal():
     if not is_login():
         return redirect(url_for('index'))
@@ -165,10 +178,11 @@ def request_banappeal():
     u = get_user()
     p, perm = get_privileges(api_user_privileges(u['user_id']))
 
-    return render_template('banappeal.html', user=get_user(), p=p, perm=perm)
+    return render_template('banappeal.html', user=api_user_username(u['user_id']), p=p,
+                           perm=perm)
 
 
-@app.route('/namechange/')
+@app.route('/namechange/', methods=['GET', 'POST'])
 def request_namechange():
     if not is_login():
         return redirect(url_for('index'))
@@ -176,8 +190,50 @@ def request_namechange():
     u = get_user()
     p, perm = get_privileges(api_user_privileges(u['user_id']))
 
-    return render_template('namechange.html', user=get_user(), p=p, perm=perm)
+    if request.method == 'POST':
 
+        username = request.form['username']
+
+        if not username:
+            flash('Username is empty!')
+
+        if username:
+            connection, cursor = mysql.connect()
+            try:
+                mysql.execute(connection, cursor,
+                              "INSERT INTO requests (user_id, username, category, used, new_username, date) VALUES (%s, %s, %s, %s, %s, %s)",
+                              [u['user_id'], api_user_username(u['user_id']), 1, 0, username,
+                               datetime.now().strftime('%d.%m.%Y %H:%M')])
+                flash('Your request is added to pending.')
+            except:
+                flash('You have still pending username change!')
+
+    return render_template('namechange.html', user=api_user_username(u['user_id']),
+                           p=p, perm=perm)
+
+@app.route('/manage/usernamechanges/')
+def manage_usernamechanges():
+
+    if not is_login():
+        return redirect(url_for('index'))
+
+    u = get_user()
+    p, perm = get_privileges(api_user_privileges(u['user_id']))
+
+    return render_template('manageusernamechanges.html', user=api_user_username(u['user_id']),
+                           p=p, perm=perm)
+
+@app.route('/manage/banappeals/')
+def manage_banappeals():
+
+    if not is_login():
+        return redirect(url_for('index'))
+
+    u = get_user()
+    p, perm = get_privileges(api_user_privileges(u['user_id']))
+
+    return render_template('managebanappeals.html', user=api_user_username(u['user_id']),
+                           p=p, perm=perm)
 
 @app.errorhandler(404)
 def not_found(error):
