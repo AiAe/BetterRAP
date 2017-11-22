c6a9010648c7fb109860a00d2ab4cf64e8730eea
Daniel Dimitrov <bgdppeu@gmail.com>
2017-11-22 20:18:24 +0200

    Should work

--- static/js/ripple.js
+++ static/js/ripple.js
@@ -1,6 +1,24 @@
-new Konami(function() {
-    var cflags = document.cookie.replace(/(?:(?:^|.*;\s*)cflags\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-    cflags = (+cflags) ^ 1;
-    document.cookie = "cflags=" + cflags + ";path=/;max-age=31536000";
-    window.location.reload();
+$(document).ready(function() {
+    $(document).keydown(function(e) {
+        var activeElement = $(document.activeElement);
+        var isInput = activeElement.is(":input,[contenteditable]");
+        if ((e.which === 83 || e.which === 115) && !isInput) {
+            $("#user-search-input").focus();
+            e.preventDefault();
+        }
+        if (e.which === 27 && isInput) {
+            activeElement.blur();
+        }
+    });
+    new Konami(function() {
+        var cflags = document.cookie.replace(/(?:(?:^|.*;\s*)cflags\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        cflags = (+cflags) ^ 1;
+        document.cookie = "cflags=" + cflags + ";path=/;max-age=31536000";
+        window.location.reload();
+    });
+    $("#language-selector .item").click(function() {
+        var lang = $(this).data("lang");
+        document.cookie = "language=" + lang + ";path=/;max-age=31536000";
+        window.location.reload();
+    });
 });
